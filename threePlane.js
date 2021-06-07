45072a5e77fe8b1ad7ae2354a65550eff1b432d7
yuzuruinoue <yuzuru1noue20@gmail.com>
2021-06-08 01:43:35 +0900

    three.js導入

--- threePlane.js
+++ threePlane.js
@@ -3,50 +3,62 @@ function init(){
 
   var camera=new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,0.1,1000);
   camera.position.x=20.0;
-  camera.position.y=30;
-  camera.position.z=20;
+  camera.position.y=500;
+  // camera.position.z=20;
 
   var renderer=new THREE.WebGLRenderer();
-  renderer.setClearColor(new THREE.Color(0x884444));
+  renderer.setClearColor(new THREE.Color(0xc5cdd1));
   renderer.setSize(window.innerWidth,window.innerHeight);
+  // renderer.setSize(document.body.clientWidth,window.innerHeight);
 
-  var boxGeom=new THREE.BoxGeometry(4,4,4);
+  var boxGeom=new THREE.BoxGeometry(300,300,300);
   var boxMat=new THREE.MeshNormalMaterial();
   var box=new THREE.Mesh(boxGeom,boxMat);
   scene.add(box);
-  camera.lookAt(box.position);
+  box.position.y=150;
+  camera.lookAt(new THREE.Vector3(0,0,0));
 
   document.getElementById("WebGL-output").appendChild(renderer.domElement);
   onwindowResize();
   window.addEventListener('resize',onwindowResize,false);
 
-  var speed=1;
-  var rightFlag=true;
+
+  let rot = 0; // 角度
+let mouseX = 0; // マウス座標
+
+// マウス座標はマウスが動いた時のみ取得できる
+document.addEventListener("mousemove", (event) => {
+  mouseX = event.pageX;
+});
+
 
   render();
 
 
   function render(){
-    if(camera.position.x>50&&rightFlag){
-      speed*=-1;
-      rightFlag=false;
-    }
-    if(camera.position.x<-50&&!rightFlag){
-      speed*=-1;
-      rightFlag=true;
-    }
-    console.log(camera.position.x);
-    console.log(rightFlag);
+    // マウスの位置に応じて角度を設定
+  // マウスのX座標がステージの幅の何%の位置にあるか調べてそれを360度で乗算する
+  const targetRot = (mouseX / window.innerWidth) * 360;
+  // イージングの公式を用いて滑らかにする
+  // 値 += (目標値 - 現在の値) * 減速値
+  rot += (targetRot - rot) * 0.02;
 
-    camera.position.x+=speed;
-    requestAnimationFrame(render);
+  // ラジアンに変換する
+  const radian = rot * Math.PI / 180;
+  // 角度に応じてカメラの位置を設定
+  camera.position.x = 1000 * Math.sin(radian);
+  camera.position.z = 1000 * Math.cos(radian);
+  // 原点方向を見つめる
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
     renderer.render(scene,camera);
+    requestAnimationFrame(render);
   }
 
   function onwindowResize(){
     camera.aspect=window.innerWidth/window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth,window.innerHeight);
+    // renderer.setSize(window.innerWidth,window.innerHeight);
+    renderer.setSize(document.body.clientWidth,window.innerHeight);
   }
 
 }
